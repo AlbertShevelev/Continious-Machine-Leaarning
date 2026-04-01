745eca123c7873ebef5f6eb43a854a7be103cf76
AlbertShevelev <shevelev984@gmail.com>
2026-04-01 18:42:32 +0900

    Commit v2

--- train.py
+++ train.py
@@ -6,10 +6,18 @@ from xgboost import XGBClassifier
 
 
 df = pd.read_csv("titanic.csv")
-df = df[['Survived', 'Pclass', 'Sex', 'Age', 'Fare']]
+df = df[['Survived', 'Pclass', 'Sex', 'Age', 'Fare', 'SibSp', 'Parch']]
 df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
 df['Age'] = df['Age'].fillna(df['Age'].median())
 
+df['Age'] = df['Age'].fillna(df['Age'].median())
+
+
+df['family_size'] = df['SibSp'] + df['Parch'] + 1
+df['is_child'] = (df['Age'] < 16).astype(int)
+
+
+df = df[['Survived', 'Pclass', 'Sex', 'Age', 'Fare', 'family_size', 'is_child']]
 
 X = df.drop('Survived', axis=1)
 y = df['Survived']
